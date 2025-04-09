850ff904627fccb3534c16e87a241ce302a1d7a8
RashidKhalique <rashidkhanjamali26@gmail.com>
2025-04-09 14:24:33 +0500

    viteconfig

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,19 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),  tailwindcss(),],
+  resolve: {
+    extensions: ['.js', '.jsx'], // Ensure Vite resolves these extensions
+},
 
 })
 
 
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
 
-// import { defineConfig } from 'vite'
-// import tailwindcss from '@tailwindcss/vite'
 // export default defineConfig({
-//   plugins: [
-//     tailwindcss(),
-//   ],
-// })
+//     plugins: [react()],
+//     resolve: {
+//         extensions: ['.js', '.jsx'], // Ensure Vite resolves these extensions
+//     },
+// });
